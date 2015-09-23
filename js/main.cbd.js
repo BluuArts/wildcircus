bd24cca0167f6306d8b3654fad81578077a89cf6
JbPasquier <contact@jbpasquier.eu>
2015-09-23 18:46:43 +0200

    Day 3
    Page News /#1

--- js/main.cbd.js
+++
@@ -1,5 +0,0 @@
-<!--#include file="vendor/jquery-1.11.2.min.js" -->
-<!--#include file="vendor/bootstrap.min.js" -->
-<!--#include file="vendor/jquery.alton.min.js" -->
-<!--#include file="main.js" -->
-<!--#include file="ga.js" -->
